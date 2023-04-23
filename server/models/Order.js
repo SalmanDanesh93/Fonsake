7a2bece9327c60ee0f10cccfb3d44b6fc37d53ae
SalmanDanesh93 <salmand64@Yahoo.com>
2023-04-23 16:33:29 -0700

    complete server models for base

---
+++ server/models/Order.js
@@ -0,0 +1,20 @@
+const mongoose = require ('mongoose');
+
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({ 
+  purchaseDate: {
+    type: Date,
+    default: Date.now
+  },
+  products: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Products'
+    }
+  ]
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
