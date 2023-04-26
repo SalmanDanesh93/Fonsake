74ca30e7db0208f3cfecf50e0166980aea44551a
SalmanDanesh93 <salmand64@Yahoo.com>
2023-04-26 15:42:10 -0700

    completed pages

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,1 +1,55 @@
-import React, { useEffect } from 'react';
+import React from "react";
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
+
+function Nav() {
+
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/orderHistory">
+              Your Fonzake Search History
+            </Link>
+          </li>
+          <li className="mx-1">
+            <a href="/" onClick={() => Auth.logout()}>
+            Fonzake Logout
+            </a>
+          </li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/signup">
+            Fonzake Signup
+            </Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/login">
+            Fonzake Login
+            </Link>
+          </li>
+        </ul>
+      );
+    }
+  }
+
+  return (
+    <header className="flex-row px-1">
+      <h1>
+        <Link to="/">
+          <span role="img" aria-label="shopping bag">🥗</span>
+        </Link>
+      </h1>
+      <nav>
+        {showNavigation()}
+      </nav>
+    </header>
+  )
+}
+
+export default Nav;
